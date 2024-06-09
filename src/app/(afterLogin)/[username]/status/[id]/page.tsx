2dcf0a19bd449a588bf3260460cd49d71a13746b
misobae <misobae0731@gmail.com>
2024-06-09 22:27:16 +0900

    feat: 사용자 게시글 상세 페이지

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,5 +1,29 @@
-export default function UserStatus() {
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
+import style from './singlePost.module.css';
+import Post from "@/app/(afterLogin)/_component/Post";
+import CommentForm from "./_component/CommentForm";
+
+export default function SinglePost() {
   return (
-    <h1>UserStatus</h1>
+    <div className={style.main}>
+      <div className={style.header}>
+        <BackButton/>
+        <h3 className={style.headerTitle}>Post</h3>
+      </div>
+      <div className={style.emptyBox}></div>
+      <Post />
+      <CommentForm />
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
   )
 }

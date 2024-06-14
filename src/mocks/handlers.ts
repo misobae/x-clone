77b0fc48f0fcf96aeb530ae9f85c93276d71f21d
misobae <misobae0731@gmail.com>
2024-06-14 13:43:43 +0900

    feat: 회원가입 폼 제출 로직 구현

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,12 @@
-import {http, HttpResponse, StrictResponse} from 'msw'
-import {faker} from "@faker-js/faker";
+import { http, HttpResponse } from 'msw'
+import { faker } from "@faker-js/faker";
 
 const User = [
   {id: 'elonmusk', nickname: 'Elon Musk', image: '/elon.jpg'},
   {id: 'misosiru', nickname: '미소시루', image: '/profile.jpg'},
   {id: 'kingcat2', nickname: '킹냥이', image: faker.image.avatar()},
 ]
+const Posts = [];
 
 export const handlers = [
   http.post('/api/login', () => {
@@ -21,6 +22,17 @@ export const handlers = [
     return new HttpResponse(null, {
       headers: {
         'Set-Cookie': 'connect.sid=;HttpOnly;Path=/;Max-Age=0'
+      }
+    })
+  }),
+  http.post('/api/users', async ({ request }) => {
+    console.log('회원가입');
+    // return HttpResponse.text(JSON.stringify('user_exists'), {
+    //   status: 403,
+    // })
+    return HttpResponse.text(JSON.stringify('ok'), {
+      headers: {
+        'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/'
       }
     })
   }),

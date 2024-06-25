79cefe9d12168290d97758a630d5d19c3f20558f
misobae <misobae0731@gmail.com>
2024-06-25 18:14:41 +0900

    feat: 이미지 상세 mock 데이터로 변경

--- src/app/(afterLogin)/@modal/[username]/status/[id]/photo/[photoId]/page.tsx
+++ src/app/(afterLogin)/@modal/[username]/status/[id]/photo/[photoId]/page.tsx
@@ -1,41 +1,43 @@
-import Post from "@/app/(afterLogin)/_component/Post";
-import CommentForm from "@/app/(afterLogin)/[username]/status/[id]/_component/CommentForm";
-import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
-import style from './photoModal.module.css';
-import PhotoModalCloseButton from "@/app/(afterLogin)/@modal/[username]/status/[id]/photo/[photoId]/_component/PhotoModalCloseButton";
-import { faker } from "@faker-js/faker";
+import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
+import { getSinglePost } from "@/app/(afterLogin)/[username]/status/[id]/_lib/getSinglePost";
+import { getComments } from "@/app/(afterLogin)/[username]/status/[id]/_lib/getComments";
 
-export default function PhotoModal() {
-  const photo = {
-    imageId: 1,
-    link: faker.image.urlLoremFlickr(),
-    Post: {
-      content: faker.lorem.text()
-    }
-  }
+import style from './photoModal.module.css';
+import CommentForm from "@/app/(afterLogin)/[username]/status/[id]/_component/CommentForm";
+
+import PhotoModalCloseButton from "@/app/(afterLogin)/@modal/[username]/status/[id]/photo/[photoId]/_component/PhotoModalCloseButton";
+import SinglePost from "@/app/(afterLogin)/[username]/status/[id]/_component/SinglePost";
+import Comments from "@/app/(afterLogin)/[username]/status/[id]/_component/Comments";
+import ImageZone from "./_component/ImgaeZone";
+
+type Props = {
+  params: { id: string }
+}
+
+export default async function PhotoModal({ params }: Props) {
+  const {id} = params;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ['posts', id], 
+    queryFn: getSinglePost
+  });
+  await queryClient.prefetchQuery({
+    queryKey: ['posts', id, 'comments'], 
+    queryFn: getComments
+  });
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <div className={style.container}>
-      <PhotoModalCloseButton />
-      <div className={style.imageZone}>
-        <img src={photo.link} alt={photo.Post?.content} />
-        <div className={style.image} style={{backgroundImage: `url(${photo.link})`}} />
-        <div className={style.buttonZone}>
-          <div className={style.buttonInner}>
-            <ActionButtons white />
-          </div>
+      <HydrationBoundary state={dehydratedState}>
+        <PhotoModalCloseButton />
+        <ImageZone id={id} />
+        <div className={style.commentZone}>
+          <SinglePost id={id} noImage />
+          <CommentForm />
+          <Comments id={id} />
         </div>
-      </div>
-      <div className={style.commentZone}>
-        <Post noImage />
-        <CommentForm />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-      </div>
+      </HydrationBoundary>
     </div>
   );
 }

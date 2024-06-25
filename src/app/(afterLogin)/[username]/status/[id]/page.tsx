bb4f03138c6b504fb4dde38986d5a6ca1fc9a389
misobae <misobae0731@gmail.com>
2024-06-25 17:53:03 +0900

    feat: user 게시글mock 데이터로 변경

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,29 +1,42 @@
-import BackButton from "@/app/(afterLogin)/_component/BackButton";
 import style from './singlePost.module.css';
-import Post from "@/app/(afterLogin)/_component/Post";
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
 import CommentForm from "./_component/CommentForm";
+import SinglePost from "./_component/SinglePost";
+import Comments from "./_component/Comments";
+import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
+import { getSinglePost } from './_lib/getSinglePost';
+import { getComments } from './_lib/getComments';
 
-export default function SinglePost() {
+type Props = {
+  params: { id: string }
+}
+export default async function Page({ params }: Props) {
+  const { id } = params;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ['posts', id],
+    queryFn: getSinglePost
+  });
+  await queryClient.prefetchQuery({
+    queryKey: ['posts', id, 'comments'], 
+    queryFn: getComments
+  });
+  const dehydratedState = dehydrate(queryClient);
+  
   return (
     <div className={style.main}>
-      <div className={style.header}>
-        <BackButton/>
-        <h3 className={style.headerTitle}>Post</h3>
-      </div>
-      <div className={style.emptyBox}></div>
-      <Post />
-      <CommentForm />
-      <div>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-      </div>
+      <HydrationBoundary state={dehydratedState}>
+        <div className={style.header}>
+          <BackButton/>
+          <h3 className={style.headerTitle}>Post</h3>
+        </div>
+        <div className={style.emptyBox}></div>
+        <SinglePost id={id} />
+        <CommentForm />
+        <div>
+          <Comments id={id} />
+        </div>
+      </HydrationBoundary>
     </div>
   )
 }
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -262,4 +262,68 @@ export const handlers = [
       status: 404,
     })
   }),
+
+  http.get('/api/posts/:postId', ({ request, params }): StrictResponse<any> => {
+    const {postId} = params;
+    if (parseInt(postId as string) > 10) {
+      return HttpResponse.json({ message: 'no_such_post' }, {
+        status: 404,
+      })
+    }
+    return HttpResponse.json(
+      {
+        postId,
+        User: User[0],
+        content: `${1} 게시글 아이디 ${postId}의 내용`,
+        Images: [
+          {imageId: 1, link: faker.image.urlLoremFlickr()},
+          {imageId: 2, link: faker.image.urlLoremFlickr()},
+          {imageId: 3, link: faker.image.urlLoremFlickr()},
+        ],
+        createdAt: generateDate(),
+      },
+    );
+  }),
+  http.get('/api/posts/:postId/comments', ({ request, params }) => {
+    const { postId } = params;
+    return HttpResponse.json(
+      [
+        {
+          postId: 1,
+          User: User[0],
+          content: `${1} 게시글 ${postId}의 답글`,
+          Images: [{imageId: 1, link: faker.image.urlLoremFlickr()}],
+          createdAt: generateDate(),
+        },
+        {
+          postId: 2,
+          User: User[0],
+          content: `${2} 게시글 ${postId}의 답글`,
+          Images: [{imageId: 1, link: faker.image.urlLoremFlickr()}],
+          createdAt: generateDate(),
+        },
+        {
+          postId: 3,
+          User: User[0],
+          content: `${3} 게시글 ${postId}의 답글`,
+          Images: [{imageId: 1, link: faker.image.urlLoremFlickr()}],
+          createdAt: generateDate(),
+        },
+        {
+          postId: 4,
+          User: User[0],
+          content: `${4} 게시글 ${postId}의 답글`,
+          Images: [{imageId: 1, link: faker.image.urlLoremFlickr()}],
+          createdAt: generateDate(),
+        },
+        {
+          postId: 5,
+          User: User[0],
+          content: `${5} 게시글 ${postId}의 답글`,
+          Images: [{imageId: 1, link: faker.image.urlLoremFlickr()}],
+          createdAt: generateDate(),
+        },
+      ]
+    )
+  }),
 ];
